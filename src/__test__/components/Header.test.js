3ff9bb736407d38b9ce868705505b9b3b37134b7
DavCardenas <c123davidleo@gmail.com>
2021-09-28 20:00:22 -0500

    added header test with store provider

---
+++ src/__test__/components/Header.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import ProviderMock from '../../__mocks__/ProviderMock';
+import Header from '../../components/Header';
+
+describe('Test Header component', () => {
+  test('Renderizado del componente', () => {
+    const header = shallow(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>,
+    );
+    expect(header.length).toEqual(1);
+  });
+
+  test('Renderizado del titulo', () => {
+    const header = mount(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>,
+    );
+    expect(header.find('.Header-title').text()).toEqual('Platzi Store');
+  });
+});
